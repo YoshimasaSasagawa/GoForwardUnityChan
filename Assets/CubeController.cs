ae5bc18cea3daa489eb1f93681091b8ca5d485c8
sasagawa yoshimasa <yo.jetm@gmail.com>
2020-08-02 16:17:03 +0900

    GoForwardUnityChan プロジェクト　初コミット＆課題提出
    チェックよろしくお願いいたします。

---
+++ Assets/CubeController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+	private float speed = -12;
+
+	private float deadLine = -10;
+
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+
+		audioSource = gameObject.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//move cube
+		transform.Translate(this.speed * Time.deltaTime, 0, 0);
+
+		//delete when it disappeared off screen
+		if (transform.position.x < this.deadLine) {
+			Destroy(gameObject);
+		}
+	}
+
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+
+		if (collision.gameObject.name == "ground")
+		{
+			audioSource.volume = 1;
+		}
+		if (collision.gameObject.name == "CubePrefab(Clone)")
+        {
+			audioSource.volume = 0.5f;
+		}
+		audioSource.Play();
+	}
+}
---
+++ Assets/UnityChanController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChanController : MonoBehaviour {
+	//compornent for animation
+	Animator animator;
+
+	//compornent for move UnityChan
+	Rigidbody2D rigid2D;
+
+	//ground position
+	private float groundLevel = -3.0f;
+
+	//reduct jump velocity
+	private float dump = 0.8f;
+
+	float jumpVelocity = 20;
+
+	private float deadLine = -9;
+
+	// Use this for initialization
+	void Start () {
+		this.animator = GetComponent<Animator>();
+		this.rigid2D = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//adjust Animator paramater in order to play running animation
+		this.animator.SetFloat("Horizontal", 1);
+
+		//check if it's ground
+		bool isGround = (transform.position.y > this.groundLevel) ? false : true;
+		this.animator.SetBool("isGround", isGround);
+
+		//ジャンプ状態でボリューム0
+		GetComponent<AudioSource>().volume = (isGround) ? 1 : 0;
+
+		//when clicked
+		if (Input.GetMouseButtonDown(0) && isGround) {
+			//get upper force
+			this.rigid2D.velocity = new Vector2(0, this.jumpVelocity);
+		}
+
+		//when not clicked
+		if (Input.GetMouseButton(0) == false) {
+			if (this.rigid2D.velocity.y > 0) {
+				this.rigid2D.velocity *= this.dump;
+			}
+		}
+
+		//デッドラインを超えたらゲームオーバーにする
+		if (transform.position.x < this.deadLine)
+        {
+			GameObject.Find("Canvas").GetComponent<UIController>().GameOver();
+			Destroy(gameObject);
+        }
+	}
+}
